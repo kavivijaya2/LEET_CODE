02d511882ffdc4501359d7cf063f5e6fd56cd9c6
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-06 14:54:56 +0530

    Time: 11 ms (53.03%) | Memory: 15.2 MB (11.91%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map_s_to_t = {}
+        map_t_to_s = {}
+
+        for ch1, ch2 in zip(s, t):
+            if ch1 in map_s_to_t:
+                if map_s_to_t[ch1] != ch2:
+                    return False
+            else:
+                map_s_to_t[ch1] = ch2
+
+            if ch2 in map_t_to_s:
+                if map_t_to_s[ch2] != ch1:
+                    return False
+            else:
+                map_t_to_s[ch2] = ch1
+
+        return True
