dc61c7f3996a5671749459d816c2a11f161ce3ac
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-06 15:08:41 +0530

    Time: 15 ms (33.19%) | Memory: 20 MB (7.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,22 @@
-from collections import Counter
- 
-class Solution(object):
-     def topKFrequent(self, nums, k):
-        
-        count= Counter(nums)
+class Solution:
+    def topKFrequent(self, nums, k):
+        count = {}
 
-        result=[]
+        # Count frequency of each number
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
 
-        for num , freq  in count.most_common(k):
-            result.append(num)
-        return result    
-        
+        # Create buckets
+        freq = [[] for i in range(len(nums) + 1)]
+
+        # Place numbers into their frequency bucket
+        for num, c in count.items():
+            freq[c].append(num)
+
+        # Collect k most frequent elements
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res        
