82a92f6a42de762741d47146909177616d8e0d25
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-06 14:08:43 +0530

    Time: 268 ms (7.08%) | Memory: 13 MB (58.34%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        cleaned = ""
+        for ch in s :
+            if ch.isalnum():
+               cleaned += ch.lower()
+        return cleaned == cleaned [::-1]
+        
