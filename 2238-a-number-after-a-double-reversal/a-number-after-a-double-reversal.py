891cf3530b021b2c1e28a7f4dd4cec650a4e23b3
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-07 19:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.30%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isSameAfterReversals(self, num):
+        if num == 0:
+            return True
+
+        if num % 10 == 0:
+            return False
+
+        return True
