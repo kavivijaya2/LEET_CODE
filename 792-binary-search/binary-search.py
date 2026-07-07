d0f3c59d0bb6568753ae4a00071cd73b246790a4
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-07 11:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.23%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums, target):
+        def binary_search(left, right):
+            if left > right:
+                return -1
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return binary_search(mid + 1, right)
+            else:
+                return binary_search(left, mid - 1)
+
+        return binary_search(0, len(nums) - 1) 
