26603c7ded904ff92897c6354d5ae46bbc03a803
kavivijaya2 <kavivijayam.ai2023@dce.edu.in>
2026-07-10 09:32:01 +0530

    Time: 5 ms (30.97%) | Memory: 12.4 MB (85.57%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,9 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        arr = sorted(nums1 + nums2)
-        n = len(arr)
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        if n % 2 == 1:
-            return float(arr[n // 2])
-        else:
-            return (arr[n // 2 - 1] + arr[n // 2]) / 2.0
+        m, n = len(nums1), len(nums2)
+        low, high = 0, m
+
+        while low <= high:
+            cut1 = (low + high) // 2
+            cut2 = (m + n + 1) // 2 - cut1
+
+            left1 = float("-inf") if cut1 == 0 else nums1[cut1 - 1]
+            right1 = float("inf") if cut1 == m else nums1[cut1]
+
+            left2 = float("-inf") if cut2 == 0 else nums2[cut2 - 1]
+            right2 = float("inf") if cut2 == n else nums2[cut2]
+
+            if left1 <= right2 and left2 <= right1:
+                if (m + n) % 2 == 0:
+                    return (max(left1, left2) + min(right1, right2)) / 2.0
+                else:
+                    return float(max(left1, left2))
+
+            elif left1 > right2:
+                high = cut1 - 1
+            else:
+                low = cut1 + 1
